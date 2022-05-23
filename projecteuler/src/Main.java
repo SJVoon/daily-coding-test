8434734179d96f346dd97a3623bca6aded21bd2c
SJVoon <sjvoon97@gmail.com>
2022-05-24 00:30:04 +0800

    Completed Project Euler Problem 1,2

---
+++ projecteuler/src/Main.java
@@ -0,0 +1,9 @@
+import solutions.page1.Problem1;
+import solutions.page1.Problem2;
+
+public class Main {
+    public static void main(String[] args){
+        Problem1.solve(1000);
+        Problem2.solve(4000000);
+    }
+}
